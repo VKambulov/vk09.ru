d15083951f485cdcf15ad2e5c2c7884b3f0267ee
VKambulov <me@vk09.ru>
2023-03-01 21:38:40 +0300

    Content in center on contact page.

---
+++ components/content/ContentCenter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+    <ContentSlot :use="$slots.content" unwrap="p"> Text </ContentSlot>
+  </div>
+</template>
+
+<style lang="ts">
+css({
+  '.text-center': {
+    textAlign: 'center',
+    my: '{space.16}',
+  }
+})
+</style>
